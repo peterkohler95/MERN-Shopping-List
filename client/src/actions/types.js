43b4aadedbb7e54018d6e32b03ca48741e4fb748
Peter K <peterkohler95@gmail.com>
2018-11-12 16:47:05 -0600

    transferred from local to github

---
+++ client/src/actions/types.js
@@ -0,0 +1,5 @@
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEM = 'ADD_ITEMS';
+export const DELETE_ITEM = 'DELETE_ITEMS';
+export const ITEMS_LOADING = 'ITEMS_LOADING';
+
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Peter:Sampras95@ds227243.mlab.com:27243/mern_shopping'
+}
